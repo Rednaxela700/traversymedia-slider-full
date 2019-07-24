023904909de0a677719f0a867719ca52129d7182
rednaxela700 <ohleg@windowslive.com>
2019-07-24 02:19:46 +0200

    declared variables, made a function clearing all images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by alexander on 24/07/2019.
  */
+let sliderImages = document.querySelectorAll('.slide'),
+    arrowRight = document.querySelector('#arrow-right'),
+    arrowLeft = document.querySelector('#arrow-left'),
+    current = 0;
+
+//Clearing all images
+function reset() {
+    for(let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = 'none';
+    }
+}
